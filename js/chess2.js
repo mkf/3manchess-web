17ea8bd5f6f60114a79fc0f6efe1fe5d8842b82d
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-04-08 21:03:26 +0200

    comments, linting

--- js/chess2.js
+++ js/chess2.js
@@ -2,92 +2,91 @@
 /*******************************
 *
 *
-*		3mannchess
+*		3manchess
 *
 *	Patryk Kisielewski
 *
 *******************************/
-const color_rim = "#ff9900";
-const color_field_1 = "#a65c13";
-const color_field_2 = "#f4d80b";
-const color_moat = "#66ff33";
+const color_rim = "#ff9900"; //kolor brzegu
+const color_field_1 = "#a65c13"; //kolor ciemnego pola
+const color_field_2 = "#f4d80b"; //kolor jasnego pola
+const color_moat = "#66ff33"; //kolor moat'a
 
-const color_field_1_light = "#003300";
-const color_field_2_light = "#009900";
+const color_field_1_light = "#003300"; //kolor podświetlonego ciemnego pola
+const color_field_2_light = "#009900"; //kolor podświetlonego jasnego pola
 /******************************/
 
-var board_radius; 
-var board_radius_rim;
-var board_radius_ring;
+var board_radius; 	// obsolete?
+var board_radius_rim; 	// promień bez brzegu
+var board_radius_ring; 	// szerokość jednego pierścienia
 
-var board;
-var ctx;
+var board; 	//obsolete?
+var ctx; 	//obsolete?
 
-var pawns;
-var pawns_radius;
+var pawns; 		//nasz obiekt
+var pawns_radius; 	//promień szachownicy wraz z brzegiem
 
-var color_rotation; //color which is on the bottom, not on the left side nor on the right side
-//potrzebny jest przycisk który będzie zmieniał tą zmienną na wybrany kolor i triggerował przerysowanie planszy
+var color_rotation; 	//color który jest na dole zwrócony w stronę usera
+//	potrzebny jest przycisk który będzie zmieniał tą zmienną na wybrany kolor i triggerował przerysowanie planszy
 
 /******************************/
 
 Number.prototype.mod = function(n) {
-	return ((this%n)+n)%n;
+	return ((this%n)+n)%n; //modulo zrobione tak żeby nie było negative
 }
 
 //window.addEventListener("load", initBoard);
 window.addEventListener("load", initPawns);
 
 function initPawns(){
-	pawns = document.getElementById('pawns');
+	pawns = document.getElementById('pawns'); 
 	if(pawns.width < pawns.height){
-		pawns_radius = pawns.width/2;
+		pawns_radius = pawns.width/2; 
 	} else {
 		pawns_radius = pawns.height/2;
 	}
-	board_radius_rim = pawns_radius-pawns_radius/20;
-	board_radius_ring = board_radius_rim/8;
+	board_radius_rim = pawns_radius-pawns_radius/20; //promień bez brzegu to promień obiektu bez 1/20 czyli szer brzegu
+	board_radius_ring = board_radius_rim/8;    //szerokość jednego pierścienia to promień_bez_brzegu / (2dziury+6ranks)
 	$('#pawns').translateCanvas({
-	  translateX: pawns_radius, translateY: pawns_radius
-	})
-	.drawArc({
-		fillStyle: color_rim,
-		layer: true,
-		name: 'rim',
-		x: 0, y: 0,
-		radius: pawns_radius,
-		start: 0, stop: 360
+		translateX: pawns_radius, translateY: pawns_radius}) 	//translacja żeby środek był (0,0)
+	.drawArc({ 			//rysuj brzeg
+		fillStyle: color_rim, 		//brzeg o kolorze brzegu
+		layer: true, 			//jest... warstwą?
+		name: 'rim', 			//nazwiemy go 'rim'
+		x: 0, y: 0, 			//środek łuku w (0,0) po translacji czyli naszym środku
+		radius: pawns_radius, 		//o promieniu całego naszego obiektu
+		start: 0, stop: 360 		//od 0 do 360 (czyli koło)
 	});
-	pawns.addEventListener ("mousemove", function (event) {
-        var x = event.clientX-pawns_radius;
-        var y = event.clientY-pawns_radius;
-		var strad = pospolar(x,y);
-		boardstrad=phiboard(strad[0]);
-		var boardpos = boardrankfile(boardstrad,strad[1])
-		if(boardpos[0] < 0 || boardpos[0] > 5){
-			boardpos = [false,false];
+	pawns.addEventListener ("mousemove", function (event) { //eventlistener na ruch myszy
+		var x = event.clientX-pawns_radius;
+		var y = event.clientY-pawns_radius; 	//współrzędne gdzie jest mysz z translacją żeby środek był (0,0)
+		var strad = pospolar(x,y); 		//strad to współrzędne biegunowe położenia myszy
+		boardstrad=phiboard(strad[0]); 		//boardstrad to kąt phi szachownicowy od osi zerowego file'a
+		var boardpos = boardrankfile(boardstrad,strad[1]) 	//pozycja szachownicowa położenia myszy
+		if(boardpos[0] < 0 || boardpos[0] > 5){ 	//jeśli !boardpos.Correct
+			boardpos = [false,false];  			//podmień współrzędne na Boolean(false)
 		}
-		
+
 		console.log("x: "+x+" y: "+y+" kąt: "+Math.floor(boardstrad*180/Math.PI)+" st. kol: "+boardpos);
-		
-		document.getElementById("col").innerHTML = boardpos[1];
+		//	daj do logu współrzędne myszy, kąt w stopniach i pozycję szachownicową
+
+		document.getElementById("col").innerHTML = boardpos[1];	//daj pozycję szachownicową do GUI
 		document.getElementById("row").innerHTML = boardpos[0];
-		
-    });
+	});
 	drawFields();
 }
 
-function drawFields(){
-        var i, j;
-		for(i = 0; i <= 23; i++){
-			for(j = 0; j <= 5; j++){
-				drawField(i, j);
-			}
+function drawFields(){ 	//funkcja dla każdego pola wzywająca drawField
+	var i, j;
+	for(i = 0; i < 24; i++){
+		for(j = 0; j < 6; j++){
+			drawField(i, j); 
 		}
-		standardColor();
+	}
+	standardColor();  //kolorowanie na standardowe, ciemny i jasny, kolory szachownicy 
 }
 
-function drawField(a, b){
+function drawField(a, b){  	//drawField(file, rank)
 	var ang_start = 8*15+15*a;
 	var ang_stop = ang_start+15;
 	var r_1 = (7-b)*board_radius_ring;
@@ -97,23 +96,23 @@ function drawField(a, b){
 		w++;
 	}
 	$('#pawns').drawPath({
-	  closed: true,
-	  layer: true,
-	  name: 'l'+a+'_'+b,
-	  x: 0, y: 0,
-	  p1: {
-		type: 'arc',
+		closed: true,
+		layer: true,
+		name: 'l'+a+'_'+b,
 		x: 0, y: 0,
-		start: ang_start, end: ang_stop,
-		radius: r_1
-	  },
-	  p2: {
-		type: 'arc',
-		x: 0, y: 0,
-		ccw: true,
-		start: ang_stop, end: ang_start,
-		radius: r_2
-	  }
+		p1: {
+			type: 'arc',
+			x: 0, y: 0,
+			start: ang_start, end: ang_stop,
+			radius: r_1
+		},
+		p2: {
+			type: 'arc',
+			x: 0, y: 0,
+			ccw: true,
+			start: ang_stop, end: ang_start,
+			radius: r_2
+		}
 	});
 }
 
@@ -137,8 +136,8 @@ function standardColor(){
 	var i, j;
 	var color;
 	var w;
-	for(i = 0; i <= 23; i++){
-		for(j = 0; j <= 5; j++){
+	for(i = 0; i < 24; i++){
+		for(j = 0; j < 6; j++){
 			w = i+1;
 			if(j%2 == 1){
 				w++;
@@ -164,7 +163,7 @@ function removeField(a, b){
 	$('#pawns').removeLayer('l'+a+'_'+b);
 }
 
-function posradius(x,y) {
+function posradius(x,y) {  //zwraca promień = √(x²+y²)
 	return Math.pow(x*x+y*y,(1/2))
 }
 
@@ -262,7 +261,7 @@ function drawBoard() {
 	ctx.arc(0,0,board_radius,0,2*Math.PI);
 	ctx.closePath();
 	ctx.fill();
-	
+
 	var i;
 	var j;
 	var licz = 0;
@@ -285,7 +284,7 @@ function drawBoard() {
 			ctx.fill();
 			licz++;
 		}
-		
+
 		if(j < 3){
 		w = Math.PI/120*((4-j)/4);
 		ctx.rotate(Math.PI/6);
