6204749fa448c9509a446ab454b59c3676ba27e4
Michał Krzysztof Feiler <feiler.michal@mensa.org.pl>
2016-04-15 18:02:48 +0200

    gameformsubmit, newgamesubmit, JSONlikeNEWGAME

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ var authdat;
 
 var klie = Client("http://platinum.edu.pl:8082/");
 
-$("#game_form").submit(function() {
+$("#game_form").submit(gameformsubmit);
+		
+var gameformsubmit = function() {
 	gameID=$("#gameid_input").val();
 
 	return false;
 }
-);
 
 $("#loginform").submit(function() {
 	var loginu=$("#logininput").val();
@@ -18,5 +19,16 @@ $("#loginform").submit(function() {
 	klie.login(loginu,passwdu,function(data) {
 		authdat=data;
 	});
-return false;
+	return false;
+});
+
+$("#newgameform").submit(function() {
+	var whiteu=$("#whitenew").val();
+	var grayu=$("#graynew").val();
+	var blacku=$("#blacknew").val();
+	klie.addGame(JSONlineNEWGAME,whiteu,grayu,blacku,function(data) {
+		$("#gameid_input").val(data.gameid);
+		gameformsubmit();
+	});
+	return false;
 });
