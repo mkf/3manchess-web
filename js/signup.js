d04b261a5662e0d9576cd9aec6ff907a8a1ff8fb
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-04-22 21:07:23 +0200

    Bot adding, authkey

--- js/signup.js
+++ js/signup.js
@@ -10,15 +10,24 @@ $("#loginform").submit(function() {
 	klie.login(loginu,passwdu,function(data) {
 		console.log(data);
 		authkey = data.authkey;
+		$("#authkey").val(authkey);
 		$("#userid").val(data.id);
 		//getting playerid
 		klie.userInfo(parseInt($("#userid").val()),function(pdata) {
 			console.log(pdata);
 			$("#playerid").val(pdata.playerid);
 		});
+		enablebotadd();
 	});
 	return false;
 });
+
+function enablebotadd() {
+	$("#whoami").prop("disabled",false);
+	$("#ownname").prop("disabled",false);
+	$("#settings").prop("disabled",false);
+	$("#loginsubmit").prop("disabled",false);
+}
 
 $("#signingform").submit(function() {
 	loginu = $("#signlogin").val();
@@ -28,8 +37,26 @@ $("#signingform").submit(function() {
 		$("#passwdu").val(passwdu);
 		console.log("signup",data);
 		authkey = data.authkey;
+		$("#authkey").val(authkey);
 		$("#userid").val(data.userid);
 		$("#playerid").val(data.playerid);
+		enablebotadd();
+	});
+	return false;
+});
+
+$("#addbotform").submit(function() {
+	var whoami = $("#whoami").val();
+	var ownname = $("#ownname").val();
+	var settings = $("#settings").val();
+	klie.newBot(whoami,ownname,{id:parseInt($("#userid").val()),authkey:authkey},settings,function(data) {
+		console.log(data);
+		$("#botid").val(data.botid);
+		$("#botplayerid").val(data.playerid);
+		$("#botauthkey").val(data.authkey);
+	});
+	return false;
+});
 	});
 	return false;
 });
